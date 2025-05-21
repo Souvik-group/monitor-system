9b73cd6afe2994e294fe9609a7369e1be23cdd31
Souvik-group <nanupatra855@gmail.com>
2025-05-22 00:33:05 +0530

    Update firebase2.js

--- src/firebase2.js
+++ src/firebase2.js
@@ -1,16 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Fixed import path
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgdEEfuLzBn95WWe8PkfVY8BEDoI4Gx8s",
-  authDomain: "parent-view-3e01a.firebaseapp.com",
-  projectId: "parent-view-3e01a",
-  storageBucket: "parent-view-3e01a.appspot.com", // Fixed storage bucket URL
-  messagingSenderId: "621929590717",
-  appId: "1:621929590717:web:a97a97e21e49c81d0b6cd4",
-  measurementId: "G-RE5T0Y7GQT",
+  apiKey: "AIzaSyBzSZowjfm-a8C6wzmcI9f8KrnfYl3lD-s",
+  authDomain: "admin-eadd9.firebaseapp.com",
+  projectId: "admin-eadd9",
+  storageBucket: "admin-eadd9.firebasestorage.app",
+  messagingSenderId: "45362433178",
+  appId: "1:45362433178:web:c997434e3ec45db9de6323",
+  measurementId: "G-JQX0X0SMMC"
 };
 
-// Initialize Firebase app for project1
-const app1 = initializeApp(firebaseConfig, "project1");
-export const db1 = getFirestore(app1); // Export Firestore instance
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
